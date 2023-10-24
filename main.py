0366dfd9bbc10ea0a2f1b4d43474db786b364403
mac <mac@Anjalis-Mac-Pro.Dlink>
2023-10-24 16:43:23 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://timesofindia.indiatimes.com/"
+
+
+def fetchAndSaveToFile(url, path):
+    r = requests.get(url)
+    with open(path, "w") as f:
+        f.write(r.text)
+
+
+fetchAndSaveToFile(url, "data/times.html")
+
+res = requests.get(url)
+print(res.json())
