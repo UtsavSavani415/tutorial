3a3c52f8fe6a7624dd4f444d2de40498a3730120
Utsav <utsavsavani415@gmail.com>
2023-10-27 01:21:13 +0530

    bs4 find method

--- 01 tags.py
+++ 01 tags.py
@@ -4,12 +4,17 @@ from bs4 import BeautifulSoup
 # with open("sample.html", "r") as f:
 #     html_doc = f.read()
 
-r = requests.get("https://webscraper.io/test-sites/e-commerce/allinone/computers")
+r = requests.get("https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets")
 
 soup = BeautifulSoup(r.text, 'lxml')
 
 print("##########################################################")
-print(soup.div)
+
+tag  = soup.find('p',{"class":"description"})
+
+atb = tag.attrs
+# print(atb["class"])
+print(tag.string)
 # print(soup.title.string, type(soup.title.string))
 
 # print(soup.find_all("div")[1])
